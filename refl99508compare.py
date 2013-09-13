676d8611ed57efecdd1769172427d1e005e7ba43
Peter Peterson <petersonpf@ornl.gov>
2013-09-13 13:09:15 -0400

    Add CheckWorkspacesMatch

--- refl99508compare.py
+++ refl99508compare.py
@@ -3,4 +3,6 @@ nxsfile=datadir+'NeXus/REF_L_99508_event.nxs'
 prefile=datadir+'preNeXus/REF_L_99508_runinfo.xml'
 
 nxs = LoadEventNexus(nxsfile)
-pre = LoadPreNexus(prefile)
+pre = LoadPreNexus(prefile, MappingFilename="/SNS/REF_L/2009_3_4B_CAL/calibrations/REF_L_TS_2010_02_19.dat", LoadMonitors=False)
+
+print "match = %s" % CheckWorkspacesMatch(pre, nxs)
