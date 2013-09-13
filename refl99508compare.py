953f8d68514a0f9805851b2184729c3ab8a3af89
Peter Peterson <petersonpf@ornl.gov>
2013-09-13 13:21:01 -0400

    Pulling out the mapping file to make the script prettier.

--- refl99508compare.py
+++ refl99508compare.py
@@ -1,8 +1,9 @@
 datadir='/SNS/REF_L/IPTS-9370/0/99508/'
 nxsfile=datadir+'NeXus/REF_L_99508_event.nxs'
 prefile=datadir+'preNeXus/REF_L_99508_runinfo.xml'
+mapfile="/SNS/REF_L/2009_3_4B_CAL/calibrations/REF_L_TS_2010_02_19.dat"
 
 nxs = LoadEventNexus(nxsfile)
-pre = LoadPreNexus(prefile, MappingFilename="/SNS/REF_L/2009_3_4B_CAL/calibrations/REF_L_TS_2010_02_19.dat", LoadMonitors=False)
+pre = LoadPreNexus(prefile, MappingFilename=mapfile, LoadMonitors=False)
 
 print "match = %s" % CheckWorkspacesMatch(pre, nxs)
