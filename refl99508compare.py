0850668db8c144325bcfd341d00ae30856ed0a3f
Peter Peterson <petersonpf@ornl.gov>
2013-09-16 16:16:47 -0400

    Adding tolerance to the compare.

--- refl99508compare.py
+++ refl99508compare.py
@@ -6,4 +6,4 @@ mapfile="/SNS/REF_L/2009_3_4B_CAL/calibrations/REF_L_TS_2010_02_19.dat"
 nxs = LoadEventNexus(nxsfile)
 pre = LoadPreNexus(prefile, MappingFilename=mapfile, LoadMonitors=False)
 
-print "match = %s" % CheckWorkspacesMatch(pre, nxs)
+print "match = %s" % CheckWorkspacesMatch(pre, nxs, Tolerance=.05)
